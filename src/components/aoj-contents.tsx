6de28c3522b7f3fa6eccc781ad867d300172246b
hppRC <hpp.ricecake@gmail.com>
2019-12-28 12:57:50 +0900

    Implementation of AOJ user data storage function in firestore

--- src/components/aoj-contents.tsx
+++ src/components/aoj-contents.tsx
@@ -1,27 +1,30 @@
 import axios, { AxiosResponse } from 'axios';
 import React, { useRef } from 'react';
-import { AOJUserContainer } from 'src/store';
+import { AOJUserContainer, FirebaseAuthContainer } from 'src/store';
 import { AOJUser } from 'types/models';
 
 import styled from '@emotion/styled';
 
 const AOJContents: React.FCX = ({ className }) => {
-  const { setAOJUser } = AOJUserContainer.useContainer();
+  const { user } = FirebaseAuthContainer.useContainer();
+  const { setAOJUserOnFirestore } = AOJUserContainer.useContainer();
 
   const ref = useRef<HTMLInputElement>(null);
 
   const onSubmit = async (e: React.SyntheticEvent<{}>) => {
     e.preventDefault();
+    if (!user) return;
     if (!ref.current) return;
 
     try {
       const url = `https://judgeapi.u-aizu.ac.jp/users/${ref.current.value}`;
       const res: AxiosResponse<AOJUser> = await axios.get(url);
-      setAOJUser(res.data);
-      console.log(res.data);
+      setAOJUserOnFirestore(user.uid, res.data);
     } catch (error) {
       console.error(error);
     }
+
+    ref.current.value = '';
   };
 
   return (
--- src/store/aoj-user-container.tsx
+++ src/store/aoj-user-container.tsx
@@ -1,11 +1,51 @@
+import firebase from 'firebase/app';
 import { useState } from 'react';
 import { AOJUser } from 'types/models';
 import { createContainer } from 'unstated-next';
 
 const useAOJUserContainer = () => {
-  const [aoj_user, setAOJUser] = useState<AOJUser | null>();
+  const [aojUser, setAOJUser] = useState<AOJUser | null>();
 
-  return { aoj_user, setAOJUser };
+  const setAOJUserOnFirestore = async (uid: string, userData: AOJUser) => {
+    if (!userData) return;
+
+    console.log('setAOJUserOnFirebase\n', userData);
+
+    const docRef = await firebase
+      .firestore()
+      .collection(`users`)
+      .doc(uid)
+      .collection(`aoj_data`)
+      .doc('userData');
+
+    const doc = await docRef.get();
+    if (doc.exists) {
+      docRef
+        .update({
+          data: userData,
+          updatedAt: firebase.firestore.FieldValue.serverTimestamp()
+        })
+        .catch(error => {
+          console.error('Error add todo to Firebase Database', error);
+        });
+    } else {
+      docRef
+        .set({
+          data: userData,
+          createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+          updatedAt: firebase.firestore.FieldValue.serverTimestamp()
+        })
+        .catch(error => {
+          console.error('Error add todo to Firebase Database', error);
+        });
+    }
+
+    setAOJUser(userData);
+
+    return;
+  };
+
+  return { aojUser, setAOJUser, setAOJUserOnFirestore };
 };
 
 export const AOJUserContainer = createContainer(useAOJUserContainer);
